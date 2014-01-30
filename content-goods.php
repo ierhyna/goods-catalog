7810af2886e0e3645d46cf8288397acd07a2dcfa
ierhyna <sokolovskaja.irina@gmail.com>
2014-01-30 13:49:57 +0600

    Create content-goods.php

---
+++ content-goods.php
@@ -0,0 +1,21 @@
+<?php
+	
+//theme_get_metadata_icons( '', 'footer' )
+	$price = get_post_meta( get_the_ID(), 'goods_price', true );
+	$descrip = get_post_meta( get_the_ID(), 'goods_second', true );
+
+	global $post;
+
+	theme_post_wrapper(
+		array(
+				'id' => theme_get_post_id(), 
+				'class' => theme_get_post_class(),
+				'thumbnail' => theme_get_post_thumbnail(),
+				'title' => '<a href="' . get_permalink( $post->ID ) . '" rel="bookmark" title="' . strip_tags(get_the_title()) . '">' . get_the_title() . '</a>', 
+        'heading' => theme_get_option('theme_'.(is_single()?'single':'posts').'_article_title_tag'),
+				'before' => theme_get_metadata_icons( '', 'header' ),
+				'content' => '<p><b>Цена: ' . $price . '</b></p>', 
+				'after' => '<p>' . $descrip . '</p>'
+		)
+	);
+?>
