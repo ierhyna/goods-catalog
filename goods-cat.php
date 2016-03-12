6a0ba519d25a4dc879cdb82f3434ff1fd2692b78
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2016-03-12 19:10:59 +0300

    fix license version

--- goods-cat.php
+++ goods-cat.php
@@ -7,8 +7,8 @@
   Version: 1.0.0
   Author: Irina Sokolovskaya
   Author URI: http://oriolo.ru/
-  License: GNU General Public License v2 or later
-  License URI: http://www.gnu.org/licenses/gpl-2.0.html
+  License: GNU General Public License v3
+  License URI: http://www.gnu.org/licenses/gpl-3.0.html
   Text Domain: goods-catalog
   Domain Path: /languages
  */
