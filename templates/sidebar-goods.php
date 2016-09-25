a7097c994115d58b36c47377d6ac64eeeb4b7a77
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2016-09-25 12:25:57 +0300

    Fix ;

--- templates/sidebar-goods.php
+++ templates/sidebar-goods.php
@@ -7,8 +7,10 @@
 global $catalog_option;
 
 if (isset($catalog_option['use_sidebar'])) {
-    echo '<aside class="goods-sidebar">';
-    if (!dynamic_sidebar('goods-sidebar')) {
+    ?>
+
+    <aside class="goods-sidebar">
+        <?php if (!dynamic_sidebar('goods-sidebar')) { ?>
 
         echo '<h3 class="widgettitle">' . __('Goods Catalog Sidebar is Activated!', 'goods-catalog') . '</h3>';
         echo __('Hi! It is Goods Catalog Sidebar. Please <a href="/wp-admin/widgets.php">add some widgets</a> in there, and this message will be hidden automatically.', 'goods-catalog');
